f892c1cbc690b21acaab6ed56b07e9a36610d3e6
xujianhong <jianhong.xu@okchem.com>
2020-01-27 01:55:17 +0800

    init

---
+++ springbootspringmvc/src/main/java/com/xjh/springboot/springmvc/SpringbootspringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.xjh.springboot.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootspringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootspringmvcApplication.class, args);
+    }
+
+}
---
+++ springbootspringmvc/src/test/java/com/xjh/springboot/springmvc/SpringbootspringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjh.springboot.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootspringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
