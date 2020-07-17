a28d5cff4fd63ae8dc81d72e6cdea3af7b1fff91
xujianhong <xujianhong@zdsoft.cn>
2020-07-17 10:24:14 +0800

    spring mvc中的cros配置 两种 一种是webMvcConfig的方式 二种是crossFilter的方式

--- springbootcode/src/main/java/com/example/springbootcode/config/WebConfig.java
+++ springbootcode/src/main/java/com/example/springbootcode/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.springbootcode.config;
 
 import com.example.springbootcode.interceptors.SecurityInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Tile: spring mvc的配置
  * @Autor: xujianhong
  * @Date: 2020/7/14 11:11
- * @Description:
+ * @Description:  实现 WebMvcConfigurer 接口 或者继承 WebMvcConfigurerAdapter
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -23,4 +24,19 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new SecurityInterceptor()).addPathPatterns("/**").excludePathPatterns("/admin/**");
     }
 
+
+    /**
+     * 设置全局跨域策略
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**")
+        .allowedOrigins("https://domain2.com")
+        .allowedMethods("PUT", "DELETE")
+        .allowedHeaders("header1", "header2", "header3")
+        .exposedHeaders("header1", "header2")
+        .allowCredentials(true).maxAge(3600);
+    }
 }
